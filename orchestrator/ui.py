07e3bb34a3f7505fc98b6f06d4374958c1d20fe6
Pedro Cazelatto <pedrocazelatto@gmail.com>
2026-04-13 23:55:44 -0300

    Change colors

--- orchestrator/ui.py
+++ orchestrator/ui.py
@@ -4,24 +4,29 @@ class UI:
     def __init__(self):
         self.console = Console()
 
+    def clear_screen(self):
+        self.console.clear()
+
+    def get_input(self):
+        self.console.print()
+        return self.console.input("[bold white on grey23] USER > [/bold white on grey23] ")
+
     def display_message(self, role, content):
         colors = {
             "system": "blue",
-            "user": "bright_white",
             "architect": "magenta",
             "dev": "cyan",
             "ai": "green"
         }
         color = colors.get(role, "white")
-
-        role_label = f"[{color}][bold]{role.upper()}>[/bold][/{color}]"
-        self.console.print(f"{role_label} {content}")
+        if role != "user":
+            self.console.print(f"\n[{color}][bold]{role.upper()} >[/bold] {content}[/{color}]")
 
     def log_action(self, action_name, details):
-        self.console.print(f"[dim yellow] {action_name}: {details}[/dim yellow]")
+        self.display_message("system", f"{action_name}: {details}")
 
     def error(self, message):
-        self.console.print(f"[bold red]❌ ERRO:[/bold red] {message}")
+        self.console.print(f"\n[bold red]X ERROR:[/bold red] {message}")
 
     def show_agent_status(self, agent_name):
-        self.console.print(f"\n[bold reverse {agent_name}] ATIVO: {agent_name.upper()} [/bold reverse {agent_name}]\n")
+        self.console.print(f"\n[bold reverse {agent_name}] ACTIVE: {agent_name.upper()} [/bold reverse {agent_name}]\n")
