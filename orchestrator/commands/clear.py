f32f414e8908927a73567553da6e09a96cc91cdd
Pedro Cazelatto <pedrocazelatto@gmail.com>
2026-04-14 23:19:19 -0300

    Delete old folder structure

--- orchestrator/commands/clear.py
+++
@@ -1,3 +0,0 @@
-def run(orc, args):
-    orc.switch_agent(orc.active_agent)
-    orc.ui.display_message("system", f"Context cleared.")
--- orchestrator/commands/switch.py
+++
@@ -1,5 +0,0 @@
-def run(orc, args):
-    if len(args) > 0 and args[0] in ["architect", "dev"]:
-        orc.switch_agent(args[0])
-    else:
-        orc.ui.error("Usage: /switch architect OR /switch dev")
